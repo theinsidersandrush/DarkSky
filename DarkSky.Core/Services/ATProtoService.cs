8d873781d63e08a8202adb54485aa6d6cfd81bca
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2024-12-11 13:21:47 +0100

    Proper login error details

--- DarkSky.Core/Services/ATProtoService.cs
+++ DarkSky.Core/Services/ATProtoService.cs
@@ -11,6 +11,8 @@ using FishyFlip.Lexicon.Com.Atproto.Server;
 using CommunityToolkit.Mvvm.Messaging;
 using DarkSky.Core.Messages;
 using CommunityToolkit.Mvvm.ComponentModel;
+using IdentityModel.OidcClient;
+using System.Diagnostics;
 
 namespace DarkSky.Core.Services
 {
@@ -25,20 +27,26 @@ namespace DarkSky.Core.Services
 			try
 			{
 				ATProtocolClient = new ATProtocolBuilder().EnableAutoRenewSession(true).Build();
-				Session = await ATProtocolClient.AuthenticateWithPasswordAsync(username, password);
-				if (Session is not null)
+				// We use CreateSessionAsync to get error message if login fails to display to the user
+				var result = await ATProtocolClient.CreateSessionAsync(username, password);
+				if (result.IsT0 && result.AsT0 is not null)
 				{
 					// code for refresh token
 					/*Session session2 = new Session(session1.Did, session1.DidDoc, session1.Handle, session1.Email, session1.AccessJwt, session1.RefreshJwt);
 					var authSession = new AuthSession(session2);
 					Session session3 = await ATProtocolClient.AuthenticateWithPasswordSessionAsync(authSession);*/
 
-					// Send authentication message
+					// Authenticate the library with the session details
+					var a = new AuthSession(new Session(result.AsT0.Did, result.AsT0.DidDoc, result.AsT0.Handle, result.AsT0.Email, result.AsT0.AccessJwt, result.AsT0.RefreshJwt));
+					Session = await ATProtocolClient.AuthenticateWithPasswordSessionAsync(a) ?? throw new Exception("AuthenticateWithPasswordSessionAsync failed, please report this to GitHub");
 					WeakReferenceMessenger.Default.Send(new AuthenticationSessionMessage(Session));
 				}
 				else
 				{
-					WeakReferenceMessenger.Default.Send(new ErrorMessage(new Exception("Failed to login Session was null (ATProtoService LoginAsync)")));
+					Debug.WriteLine(result.AsT1.StatusCode);
+					Debug.WriteLine(result.AsT1.Detail?.Message);
+					Debug.WriteLine(result.AsT1.Detail?.Error);
+					WeakReferenceMessenger.Default.Send(new ErrorMessage(new Exception($"{result.AsT1.Detail?.Message} - {result.AsT1.StatusCode} {result.AsT1.Detail?.Error}")));
 				}
 			}
 			catch (Exception ex) {
